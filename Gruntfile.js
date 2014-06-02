7d1b42f83330b473f4c1cf70374591abe9e34878
Ryo TANAKA <ryotan@ucb-jp.net>
2014-06-03 00:17:26 +0900

    Add compass via bundler.

--- Gruntfile.js
+++ Gruntfile.js
@@ -170,7 +170,8 @@ module.exports = function (grunt) {
         httpFontsPath: '/styles/fonts',
         relativeAssets: false,
         assetCacheBuster: false,
-        raw: 'Sass::Script::Number.precision = 10\n'
+        raw: 'Sass::Script::Number.precision = 10\n',
+        bundleExec: true
       },
       dist: {
         options: {
