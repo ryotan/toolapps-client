fd01e48a2a65c743bc6b063ae881f26042747a0a
Ryo TANAKA <ryotan@ucb-jp.net>
2014-06-08 14:07:05 +0900

    Initialize converted string as empty.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,7 @@
 angular.module('toolappsClientApp')
   .controller('MainCtrl', function ($scope) {
     $scope.target = '';
+    $scope.converted = '';
     $scope.convert = function() {
       $scope.converted = $scope.target;
     };
